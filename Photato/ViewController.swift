fa0e7afa69137ecd01fc7b057b82be60ab5bb030
Alexander <alexandersivko.swift@gmail.com>
2023-08-05 15:06:21 +0300

    Initial Commit

---
+++ Photato/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photato
+//
+//  Created by Alexander Strelnikov on 5.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
