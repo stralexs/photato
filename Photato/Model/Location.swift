28a993f28a6bb9764ef43049a182b2b1d6b57743
Alexander <alexandersivko.swift@gmail.com>
2023-08-05 19:59:05 +0300

    Added Location model with tests

---
+++ Photato/Model/Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  Photato
+//
+//  Created by Alexander Sivko on 5.08.23.
+//
+
+import Foundation
+
+struct Location {
+    struct Coordinates: Equatable {
+        var longitude: Double
+        var latitude: Double
+    }
+    
+    var name: String
+    var description: String
+    var address: String
+    var coordinates: Coordinates
+    var imagesData: [Data]
+}
+
