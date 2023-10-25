fa96972070cfcf7ca73d809259a12574c1ae3a23
Alexander <alexandersivko.swift@gmail.com>
2023-10-25 11:05:18 +0300

    Code formatting

---
+++ Photato/WeatherForecast/HourlyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyCollectionViewCell.swift
+//  Photato
+//
+//  Created by Alexander Strelnikov on 29.09.23.
+//
+
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    
+}
